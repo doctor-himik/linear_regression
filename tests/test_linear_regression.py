5a9bc484a95f9bb4c2edf60ecb0d8627dff0cc45
doctor-himik <102741469+doctor-himik@users.noreply.github.com>
2025-09-24 20:51:54 +0300

    Add files via upload

---
+++ tests/test_linear_regression.py
@@ -0,0 +1,18 @@
+import pytest
+
+
+class TestLinearRegressionWithGradientDescent:
+    def setup_class(self):
+        ...
+
+    def teardown_class(self):
+        ...
+
+    def test_happy_path(self):
+        ...
+
+    def test_base_positive(self):
+        ...
+
+    def test_base_negative(self):
+        ...
